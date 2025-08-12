8347d935ea42d6b5869c1641162e2e8d35bbc3fe
Rohaan Khan <rohaanahmed181@gmail.com>
2025-08-12 15:44:44 +0500

    This is a simple C program that welcomes the user with a custom message. It uses basic input/output and a simple if statement to display the result.

---
+++ Class 6 if.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+	
+	char name[50]= "ANONYMOUS";
+	printf("Welcome Anonymous");
+	scanf("%s", &name);
+	
+	if(name > "ANONYMOUS"){
+		printf("Welcome Anonymous %s ", name);
+		
+	}
+	
+	
+	return 0;
+	
+}  
+	
+	
+
